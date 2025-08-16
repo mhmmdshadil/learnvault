69cb3afd7037f2b484ac7c89cb00eba64f167ddb
Muhammed Shadil <mhmmd.shadil.mp@gmail.com>
2025-08-16 18:28:43 +0530

    Added sw.js for PWA functionality

---
+++ sw.js
@@ -0,0 +1,15 @@
+// sw.js
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('learnvault-cache').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
